b70d18e3f2ca63ef354a35252ac8d2a097ecd5fe
Júnior Silva <junior_dariana@hotmail.com>
2017-05-09 00:06:55 -0300

    2° commit

---
+++ SystemXML_Example/Form3.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+using System.Xml.Serialization;
+
+namespace SystemXML_Example
+{
+    public partial class Form3 : Form
+    {
+        public Form3()
+        {
+            InitializeComponent();
+        }
+
+        private void Form3_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ SystemXML_Example/Serializer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using System.Xml.Serialization;
+
+namespace SystemXML_Example
+{
+    public static class Serializer
+    {
+        public static T Deserialize<T>(this XElement xElement)
+        {
+            using (var memoryStream = new MemoryStream(Encoding.ASCII.GetBytes(xElement.ToString())))
+            {
+                var XmlSerealizer = new XmlSerializer(typeof(T));
+                return (T)XmlSerealizer.Deserialize(memoryStream);
+            }
+        }
+
+        public static XElement Serealize<T>(this object o)
+        {
+            using (var memoryStream = new MemoryStream())
+            {
+                using (TextWriter streamWritter = new StreamWriter(memoryStream))
+                {
+                    var xmlSerializer = new XmlSerializer(typeof(T));
+                    xmlSerializer.Serialize(streamWritter, o);
+                    return XElement.Parse(Encoding.ASCII.GetString(memoryStream.ToArray());
+                }
+            }
+        }
+    }
+}
